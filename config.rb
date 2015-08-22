bc61878959e5611479b42ae5ceb765e476985321
Teng-hao Chang <zhusee2@gmail.com>
2015-08-22 22:00:40 +0800

    Config autoprefixer and build-settings

--- config.rb
+++ config.rb
@@ -36,9 +36,9 @@
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+  activate :livereload
+end
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -48,21 +48,26 @@
 # end
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
+
+# Setup Google Analytics
+# activate :google_analytics do |ga|
+#   ga.tracking_id = 'UA-xxxxxxx-3'
+# end
+
+# Autoprefixer for automatic vendor-prefixes
+activate :autoprefixer do |config|
+  config.browsers = ['last 2 versions', 'Explorer >= 10']
+end
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_css
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
